47007f2677c2649dc220ddb7af06a29e1d3169c2
BernardoMB <bmondragonbrozon@gmail.com>
2021-05-03 21:27:07 -0500

    Added continuity correction comparison plot for Poisson Distribution

--- Continuity correction/Poisson Distribution/Continuity correction comparison.R
+++ Continuity correction/Poisson Distribution/Continuity correction comparison.R
@@ -10,18 +10,17 @@ if (bw) {
   colors <- c('goldenrod1', 'darkorange2', 'darkorange4', 'pink', 'deeppink1', 'purple')
 }
 
-# n: Number of trials
-n <- 10
-# p: Probability of success
-p <- 1/2
+# l: Expected value of Poisson Distribution
+l <- 15
+lim <- 40
 
 # X intercept
-x.intercept <- 4
+x.intercept <- 18
 vertical.line <- geom_vline(xintercept = x.intercept, linetype = 12, color = colors[1], size = 1)
 
 # --- CDF ----
-sup <- seq(0,n,1)
-cdf <- pbinom(sup, size = n, prob = p)
+sup <- seq(0,lim,1)
+cdf <- ppois(sup, lambda = l)
 cdf.data <- data.frame(sup, cdf, xend = sup + 1, yend = cdf)
 
 scaleFUN <- function(x) sprintf("%.2f", x)
@@ -29,7 +28,7 @@ cdf.plot <- ggplot(cdf.data, aes(x = sup, y = cdf)) +
   geom_point() +
   geom_point(aes(x = sup + 1, y = cdf), shape = 21, fill = "white") +
   geom_segment(aes(x = sup, y = cdf, xend = xend, yend = yend)) +
-  scale_x_continuous(breaks=0:n, limits = c(0, n)) +
+  scale_x_continuous(breaks=0:lim, limits = c(0, lim)) +
   scale_y_continuous(labels=scaleFUN) +
   labs(x = "Value of x", y = "F(x)") +
   theme_bw() +
@@ -38,7 +37,7 @@ graph <- cdf.plot
 graph
 
 # Create CDF layer # Normal aproximation
-cdf.normal <- function(x, mean = n*p, sd = sqrt(n*p*(1-p))) {
+cdf.normal <- function(x, mean = l, sd = sqrt(l)) {
   pnorm(x, mean = mean, sd = sd)
 }
 cdf.plot.normal <- stat_function(fun = cdf.normal, color = colors[2])
@@ -46,7 +45,7 @@ graph <- graph + cdf.plot.normal
 graph
 
 # Create CDF layer # Normal aproximation with correction < x
-cdf.normal.correction <- function(x, mean = n*p, sd = sqrt(n*p*(1-p))) {
+cdf.normal.correction <- function(x, mean = l, sd = sqrt(l)) {
   pnorm(x - 0.5, mean = mean, sd = sd)
 }
 cdf.plot.normal.correction <- stat_function(fun = cdf.normal.correction, color = colors[3], linetype = "dashed", size = 1)
@@ -54,7 +53,7 @@ graph <- graph + cdf.plot.normal.correction
 graph
 
 # Create CDF layer # Normal aproximation with correction <= x
-cdf.normal.correction.2 <- function(x, mean = n*p, sd = sqrt(n*p*(1-p))) {
+cdf.normal.correction.2 <- function(x, mean = l, sd = sqrt(l)) {
   pnorm(x + 0.5, mean = mean, sd = sd)
 }
 cdf.plot.normal.correction.2 <- stat_function(fun = cdf.normal.correction.2, color = colors[4], linetype = "dotdash", size = 1)
@@ -65,9 +64,9 @@ graph <- graph + vertical.line
 graph
 
 # ---- PMF ----
-sup <- seq(0,n,1)
-pmf <- dbinom(sup, size = n, prob = p)
-pmf.data <- data.frame(sup, pmf, yend = rep(0,(n+1)))
+sup <- seq(0,lim,1)
+pmf <- dpois(sup, lambda = l)
+pmf.data <- data.frame(sup, pmf, yend = rep(0,(lim+1)))
 
 graph2 <- ggplot(pmf.data, aes(x = sup, y = pmf)) +
   geom_col(stat="identity", color="black", fill="white", width = 1, size = 1) +
@@ -79,7 +78,7 @@ graph2 <- ggplot(pmf.data, aes(x = sup, y = pmf)) +
     size = 3,
     vjust =  0
   ) +
-  scale_x_continuous(breaks=0:n, limits = c(0, n)) +
+  scale_x_continuous(breaks=0:lim, limits = c(0, lim)) +
   scale_y_continuous(labels=scaleFUN) +
   labs(x = "Value of x",
        y = "P(x) vs. f(x)") +
@@ -89,7 +88,7 @@ graph2 <- ggplot(pmf.data, aes(x = sup, y = pmf)) +
 graph2
 
 # Create CDF layer # Normal aproximation
-pdf.normal <- function(x, mean = n*p, sd = sqrt(n*p*(1-p))) {
+pdf.normal <- function(x, mean = l, sd = sqrt(l)) {
   dnorm(x, mean = mean, sd = sd)
 }
 pdf.plot.normal <- stat_function(fun = pdf.normal, color = colors[2])
@@ -97,7 +96,7 @@ graph2 <- graph2 + pdf.plot.normal
 graph2
 
 # Create CDF layer # Normal aproximation < x
-pdf.normal.correction <- function(x, mean = n*p, sd = sqrt(n*p*(1-p))) {
+pdf.normal.correction <- function(x, mean = l, sd = sqrt(l)) {
   dnorm(x - 0.5, mean = mean, sd = sd)
 }
 pdf.plot.normal.correction <- stat_function(fun = pdf.normal.correction, color = colors[3], linetype = "dashed", size = 1)
@@ -105,7 +104,7 @@ graph2 <- graph2 + pdf.plot.normal.correction
 graph2
 
 # Create CDF layer # Normal aproximation <= x
-pdf.normal.correction.2 <- function(x, mean = n*p, sd = sqrt(n*p*(1-p))) {
+pdf.normal.correction.2 <- function(x, mean = l, sd = sqrt(l)) {
   dnorm(x + 0.5, mean = mean, sd = sd)
 }
 pdf.plot.normal.correction.2 <- stat_function(fun = pdf.normal.correction.2, color = colors[4], linetype = "dotdash", size = 1)
@@ -116,4 +115,28 @@ graph2 <- graph2 + vertical.line
 graph2
 
 # ---- Grid ----
-grid.arrange(graph, graph2, nrow = 2, ncol = 1, layout_matrix = rbind(c(1), c(2)), top = textGrob("Binomial Distribution vs. Normal Approximation", gp = gpar(fontsize = 14)))
+grid.arrange(graph, graph2, nrow = 2, ncol = 1, layout_matrix = rbind(c(1), c(2)), top = textGrob("Poisson Distribution vs. Normal Approximation", gp = gpar(fontsize = 14)))
+
+aprox.data.1 <- data.frame(
+  sup, 
+  ppois(sup-1, lambda = l), 
+  cdf.normal(sup),
+  abs(ppois(sup-1, lambda = l) - cdf.normal(sup)),
+  cdf.normal.correction(sup),
+  abs(ppois(sup-1, lambda = l) - cdf.normal.correction(sup))  
+)
+names(aprox.data.1) <- c('x', 'P(X<x)', 'P(N<x)', 'Err P(N<x)', 'P(N<x+0.5)', 'Err P(N<x+0.5)')
+aprox.data.1
+
+aprox.data.2 <- data.frame(
+  sup, 
+  ppois(sup, lambda = l), 
+  cdf.normal(sup),
+  abs(ppois(sup, lambda = l) - cdf.normal(sup)),
+  cdf.normal.correction.2(sup),
+  abs(ppois(sup, lambda = l) - cdf.normal.correction.2(sup))  
+)
+names(aprox.data.2) <- c('x', 'P(X<=x)', 'P(N<=x)', 'Err P(N<=x)', 'P(N<=x+0.5)', 'Err P(N<x+0.5)')
+aprox.data.2
+
+
